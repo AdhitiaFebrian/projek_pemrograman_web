500c3c225ee14969a35770b0817dd98ad6e95aba
AdhitiaFebrian <stonesullivan445@gmail.com>
2025-07-16 01:51:00 +0700

    Commit 19: Menampilkan pesan error validasi di bawah masing-masing field input

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -5,30 +5,28 @@
 @section('content')
     <h1>Tambah Buku</h1>
 
-    {{-- Tampilkan validasi error --}}
-    @if ($errors->any())
-        <div style="color: red;">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
     <form action="{{ route('books.store') }}" method="POST">
         @csrf
         <div>
             <label for="judul">Judul:</label>
             <input type="text" name="judul" value="{{ old('judul') }}">
+            @error('judul')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
         <div>
             <label for="penulis">Penulis:</label>
             <input type="text" name="penulis" value="{{ old('penulis') }}">
+            @error('penulis')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
         <div>
             <label for="stok">Stok:</label>
             <input type="number" name="stok" value="{{ old('stok') }}">
+            @error('stok')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
         <br>
         <button type="submit">Simpan</button>
--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -17,22 +17,32 @@
     @endif
 
     <form action="{{ route('books.update', $book->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <div>
-            <label for="judul">Judul:</label>
-            <input type="text" name="judul" value="{{ old('judul', $book->judul) }}">
-        </div>
-        <div>
-            <label for="penulis">Penulis:</label>
-            <input type="text" name="penulis" value="{{ old('penulis', $book->penulis) }}">
-        </div>
-        <div>
-            <label for="stok">Stok:</label>
-            <input type="number" name="stok" value="{{ old('stok', $book->stok) }}">
-        </div>
-        <br>
-        <button type="submit">Perbarui</button>
-        <a href="{{ route('books.index') }}">Batal</a>
-    </form>
+    @csrf
+    @method('PUT')
+    <div>
+        <label for="judul">Judul:</label>
+        <input type="text" name="judul" value="{{ old('judul', $book->judul) }}">
+        @error('judul')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
+    </div>
+    <div>
+        <label for="penulis">Penulis:</label>
+        <input type="text" name="penulis" value="{{ old('penulis', $book->penulis) }}">
+        @error('penulis')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
+    </div>
+    <div>
+        <label for="stok">Stok:</label>
+        <input type="number" name="stok" value="{{ old('stok', $book->stok) }}">
+        @error('stok')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
+    </div>
+    <br>
+    <button type="submit">Perbarui</button>
+    <a href="{{ route('books.index') }}">Batal</a>
+</form>
+
 @endsection
