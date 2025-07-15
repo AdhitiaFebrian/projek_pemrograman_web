987ecc3130fb5e49c30a98a8b14e4cb2851496b7
AdhitiaFebrian <stonesullivan445@gmail.com>
2025-07-16 01:19:05 +0700

    Commit 10: Menampilkan pesan sukses setelah buku dihapus di index.blade.php

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -6,39 +6,52 @@
 <body>
     <h1>Daftar Buku</h1>
 
+    {{-- Pesan sukses (misalnya setelah tambah/edit/hapus) --}}
     @if (session('success'))
-        <div style="color: green">
+        <div style="color: green; margin-bottom: 15px;">
             {{ session('success') }}
         </div>
     @endif
 
+    {{-- Tombol Tambah Buku --}}
     <a href="{{ route('books.create') }}">Tambah Buku</a>
 
-    <table border="1" cellpadding="8" cellspacing="0">
-        <tr>
-            <th>No</th>
-            <th>Judul</th>
-            <th>Penulis</th>
-            <th>Stok</th>
-            <th>Aksi</th>
-        </tr>
-        @foreach ($books as $index => $book)
-        <tr>
-            <td>{{ $index + 1 }}</td>
-            <td>{{ $book->judul }}</td>
-            <td>{{ $book->penulis }}</td>
-            <td>{{ $book->stok }}</td>
-            <td>
-                <a href="{{ route('books.edit', $book->id) }}">Edit</a>
+    <br><br>
 
-                <form action="{{ route('books.destroy', $book->id) }}" method="POST" style="display:inline" onsubmit="return confirm('Yakin ingin hapus?')">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Hapus</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
+    {{-- Tabel Buku --}}
+    <table border="1" cellpadding="8" cellspacing="0">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Judul</th>
+                <th>Penulis</th>
+                <th>Stok</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($books as $index => $book)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $book->judul }}</td>
+                    <td>{{ $book->penulis }}</td>
+                    <td>{{ $book->stok }}</td>
+                    <td>
+                        <a href="{{ route('books.edit', $book->id) }}">Edit</a>
+
+                        <form action="{{ route('books.destroy', $book->id) }}" method="POST" style="display:inline" onsubmit="return confirm('Yakin ingin menghapus buku ini?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Hapus</button>
+                        </form>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="5" style="text-align: center;">Tidak ada data buku</td>
+                </tr>
+            @endforelse
+        </tbody>
     </table>
 </body>
 </html>
