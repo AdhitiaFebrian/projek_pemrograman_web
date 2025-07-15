7b5add2c23e9d6a7ae63d229554cd1db818252c8
AdhitiaFebrian <stonesullivan445@gmail.com>
2025-07-16 01:21:29 +0700

    Commit 11: Menambahkan placeholder, autofocus, dan perbaikan tampilan form create/edit

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -7,7 +7,7 @@
     <h1>Tambah Buku</h1>
 
     @if ($errors->any())
-        <div style="color: red">
+        <div style="color: red; margin-bottom: 10px;">
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -16,22 +16,17 @@
         </div>
     @endif
 
-    @if (session('success'))
-        <div style="color: green">
-            {{ session('success') }}
-        </div>
-    @endif
-
     <form action="{{ route('books.store') }}" method="POST">
         @csrf
+
         <label>Judul:</label><br>
-        <input type="text" name="judul" value="{{ old('judul') }}"><br><br>
+        <input type="text" name="judul" value="{{ old('judul') }}" placeholder="Masukkan judul buku" autofocus><br><br>
 
         <label>Penulis:</label><br>
-        <input type="text" name="penulis" value="{{ old('penulis') }}"><br><br>
+        <input type="text" name="penulis" value="{{ old('penulis') }}" placeholder="Masukkan nama penulis"><br><br>
 
         <label>Stok:</label><br>
-        <input type="number" name="stok" value="{{ old('stok') }}"><br><br>
+        <input type="number" name="stok" value="{{ old('stok') }}" placeholder="Masukkan jumlah stok"><br><br>
 
         <button type="submit">Simpan</button>
     </form>
--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -7,7 +7,7 @@
     <h1>Edit Buku</h1>
 
     @if ($errors->any())
-        <div style="color: red">
+        <div style="color: red; margin-bottom: 10px;">
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -16,24 +16,18 @@
         </div>
     @endif
 
-    @if (session('success'))
-        <div style="color: green">
-            {{ session('success') }}
-        </div>
-    @endif
-
     <form action="{{ route('books.update', $book->id) }}" method="POST">
         @csrf
         @method('PUT')
 
         <label>Judul:</label><br>
-        <input type="text" name="judul" value="{{ old('judul', $book->judul) }}"><br><br>
+        <input type="text" name="judul" value="{{ old('judul', $book->judul) }}" placeholder="Masukkan judul buku" autofocus><br><br>
 
         <label>Penulis:</label><br>
-        <input type="text" name="penulis" value="{{ old('penulis', $book->penulis) }}"><br><br>
+        <input type="text" name="penulis" value="{{ old('penulis', $book->penulis) }}" placeholder="Masukkan nama penulis"><br><br>
 
         <label>Stok:</label><br>
-        <input type="number" name="stok" value="{{ old('stok', $book->stok) }}"><br><br>
+        <input type="number" name="stok" value="{{ old('stok', $book->stok) }}" placeholder="Masukkan jumlah stok"><br><br>
 
         <button type="submit">Simpan</button>
     </form>
