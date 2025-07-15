3db2bd2a11efd5fc985e1a541ed1520ef66639b7
AdhitiaFebrian <stonesullivan445@gmail.com>
2025-07-16 01:47:38 +0700

    Commit 18: Menambahkan flash message sukses dan error di layout utama

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,25 +17,33 @@
         }
         .alert {
             padding: 10px;
-            background-color: #c6f6d5;
-            border: 1px solid #38a169;
             margin-bottom: 15px;
         }
     </style>
 </head>
 <body>
     <div class="container">
+        {{-- Navigasi --}}
         <nav>
             <a href="{{ route('books.index') }}">Home</a> |
             <a href="{{ route('books.create') }}">Tambah Buku</a>
         </nav>
 
+        {{-- Pesan Sukses --}}
         @if (session('success'))
-            <div class="alert">
+            <div class="alert" style="background-color: #c6f6d5; border: 1px solid #38a169;">
                 {{ session('success') }}
             </div>
         @endif
 
+        {{-- Pesan Error --}}
+        @if (session('error'))
+            <div class="alert" style="background-color: #fed7d7; border: 1px solid #e53e3e;">
+                {{ session('error') }}
+            </div>
+        @endif
+
+        {{-- Konten Utama --}}
         @yield('content')
     </div>
 </body>
